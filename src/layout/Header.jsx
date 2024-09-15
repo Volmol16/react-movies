45ea6f4a3fa183f17703158a336b80d344069a4e
Олег <109240899+Volmol16@users.noreply.github.com>
2024-09-15 18:56:52 +0300

    Update Header.jsx

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
     
   <nav className='green lighten-1'>
   <div className="nav-wrapper">
-    <a href="!#" className="brand-logo">React Movies</a>
+    <a href="/" className="brand-logo">React Movies</a>
     <ul id="nav-mobile" className="right hide-on-med-and-down">
       <li><a href="sass.html">Repository</a></li>
     </ul>
